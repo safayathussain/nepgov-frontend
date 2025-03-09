87a57a729031adbc770132024c8d6fcf6768408a
safayathussain <safayatofficial1@gmail.com>
2025-03-10 05:27:34 +0600

    build issue fix

--- utils/constants.js
+++ utils/constants.js
@@ -1,2 +1,5 @@
 export const ImgUrl = process.env.NEXT_PUBLIC_BASE_IMAGE_API
-export const isCookieAccepted = localStorage.getItem("acceptCookie") || sessionStorage.getItem("acceptCookie")
+export const isCookieAccepted =
+  typeof window !== "undefined"
+    ? localStorage.getItem("acceptCookie") || sessionStorage.getItem("acceptCookie")
+    : null;
